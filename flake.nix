08faf026acd60482926a76ffd0e89300cb437559
Thomas Laich <thomaslaich@gmail.com>
2024-05-10 11:06:41 +0200

    update packages

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description =
-    "A flake that provides an overlay for emacs packages in order to use plugins unavailable in nixpkgs.emacsPackages / MELPA";
+  description = "A flake that provides an overlay for emacs packages in order to use plugins unavailable in nixpkgs.emacsPackages / MELPA";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -21,8 +20,17 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, copilot-el, jsonrpc-el, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      copilot-el,
+      jsonrpc-el,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
 
@@ -30,7 +38,7 @@
         jsonrpc = pkgs.emacsPackages.elpaBuild {
           pname = "jsonrpc";
           ename = "jsonrpc";
-          version = "1.0.24";
+          version = "1.0.25";
           src = "${jsonrpc-el}/jsonrpc.el";
           packageRequires = [ pkgs.emacs ];
         };
@@ -38,33 +46,36 @@
         # copilot.el is not on MELPA, so we need to build it ourselves
         copilot = pkgs.emacsPackages.trivialBuild {
           pname = "copilot";
-          version = "2023-11-08";
-          packageRequires = with pkgs.emacsPackages; [
-            dash
-            editorconfig
-            s
-            jsonrpc
-            f
-          ];
+          version = "2024-05-01";
+          packageRequires =
+            with pkgs.emacsPackages;
+            [
+              dash
+              editorconfig
+              s
+              f
+            ]
+            ++ [ jsonrpc ];
           preInstall = ''
             mkdir -p $out/share/emacs/site-lisp
-            cp -vr $src/dist $out/share/emacs/site-lisp
+            cp -vr $src $out/share/emacs/site-lisp
           '';
           src = copilot-el;
           meta.homepage = "https://github.com/zerolfx/copilot.el/";
         };
-
-      in {
+      in
+      {
         packages = {
           inherit copilot;
           inherit jsonrpc;
         };
-      }) // {
-        overlays.default = final: prev: {
-          emacsPackages = prev.emacsPackages // {
-            inherit (self.packages.${prev.system}) copilot jsonrpc;
-          };
+      }
+    )
+    // {
+      overlays.default = final: prev: {
+        emacsPackages = prev.emacsPackages // {
+          inherit (self.packages.${prev.system}) copilot jsonrpc;
         };
       };
+    };
 }
-
